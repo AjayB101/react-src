bdb7af06702aa0663e65031af243199ab6df8621
AJAY-751 <ajay.b.civil.2019@snsct.org>
2023-05-25 10:22:14 +0530

    bugs

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,67 @@
+import { Box, Menu, Switch } from "@mui/material";
+import React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { Home } from "@mui/icons-material";
+
+export const Sidebar = () => {
+  return (
+    <Box
+      flex={1}
+      p={2}
+      sx={{
+        display: { xs: "none", sm: "block" },
+        backgroundColor: "whitesmoke",
+         width:'250px'
+      }}
+    >
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href="#smaple">
+            <ListItemIcon>
+              <Home color="white" />
+            </ListItemIcon>
+            <ListItemText primary="HomePage" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href="#smaple">
+            <ListItemIcon>
+              <Home color="white" />
+            </ListItemIcon>
+            <ListItemText primary="pages" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href="#smaple">
+            <ListItemIcon>
+              <Home color="white" />
+            </ListItemIcon>
+            <ListItemText primary="Group" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href="#smaple">
+            <ListItemIcon>
+              <Home color="white" />
+            </ListItemIcon>
+            <ListItemText primary="MarketPlace" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href="#smaple">
+            <ListItemIcon>
+              <Home color="white" />
+            </ListItemIcon>
+            <ListItemText primary="HomePage" />
+          </ListItemButton>
+        </ListItem>
+       
+
+      </List>
+    </Box>
+  );
+};
